4c8b0f147841a30f61bdbc28df14ae5836bc0b7e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2022-07-13 16:38:15 +0530

    Remove db

--- plugins/vars.py
+++ plugins/vars.py
@@ -3,5 +3,4 @@ from plugins.admin import Database
 
 BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 DATABASE = os.environ.get("DATABASE_URL")
-db = Database(DATABASE)
 DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
